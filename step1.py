421de114678a132badb9f59f1af04b84746cdd23
Phantomfra <francesca.arrigoni99@gmail.com>
2023-07-14 16:15:09 +0200

    Fixes to step1 and respective learners

--- step1.py
+++ step1.py
@@ -8,44 +8,44 @@ from Customer import Customer
 from plotResults import plot
 from clairvoyant import getOptimal
 
+c1=(Customer('C1', -0.0081, 0.97, 32, 3.8, -1.5, 0.1, 100))
 
-c1 = Customer('C1', -0.0081, 0.97, 32, 3.8, -1.5, 0.1, 100)
 prices = [10, 20, 30, 40, 50]
 prices=np.array(prices)
 T = 365
-n_experiments = 100
-norm_gain=[]
+n_experiments = 10
 
-prb=c1.conversion_probability(np.array(prices))
-gain=(prices-8)*prb
-norm_gain=gain/np.sqrt(np.sum(gain**2))
-c=gain[0]/norm_gain[0]
+#prb=c1.conversion_probability(np.array(prices))
+prb=np.array([0.95,0.75,0.53,0.28,0.14])
+margin=(prices-8)
+clicks=int(np.array(c1.num_clicks(2)))
+cost=np.array(c1.click_cost(2))
 
 ucb1_rewards_per_experiment = []
 ts_rewards_per_experiment = []
 
 for e in range(0, n_experiments):
-    env = PricingEnvironment(prices, [c1], norm_gain)
-    ucb1_learner = UCB1_Learner(len(prices), prices)
-    ts_learner = TS_Learner(len(prices), prices)
+    env = PricingEnvironment(prices, [c1], prb)
+    ucb1_learner = UCB1_Learner(len(prices), prices,margin,clicks,cost)
+    ts_learner = TS_Learner(len(prices), prices,margin,clicks,cost)
 	
     for t in range(0,T):
         # UCB1
         pulled_arm = ucb1_learner.pull_arm()
-        reward = env.round(pulled_arm)[0]
+        reward = env.round(pulled_arm,clicks)[0]
         ucb1_learner.update(pulled_arm, reward)
 
         # Thompson Sampling Learner
         pulled_arm = ts_learner.pull_arm()
-        reward = env.round(pulled_arm)[0]
+        reward = env.round(pulled_arm,clicks)[0]
         ts_learner.update(pulled_arm, reward)
 
     ts_rewards_per_experiment.append(ts_learner.collected_rewards)
     ucb1_rewards_per_experiment.append(ucb1_learner.collected_rewards)
 
-ts_rewards_per_experiment = np.array(ts_rewards_per_experiment)*c
-ucb1_rewards_per_experiment = np.array(ucb1_rewards_per_experiment)*c
+ts_rewards_per_experiment = np.array(ts_rewards_per_experiment)
+ucb1_rewards_per_experiment = np.array(ucb1_rewards_per_experiment)
 
-opt = 11.79
+opt=1008.17
 
 plot(opt, T, [ucb1_rewards_per_experiment, ts_rewards_per_experiment], ['UCB1', 'TS'])
